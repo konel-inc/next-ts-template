5919a3f6ba1372383c111eeda2629172b9817458
jay <jayq8805@gmail.com>
2022-04-25 06:41:35 +0900

    update(module, eslint, app): Styled-componentsモジュールの設置

---
+++ src/styles/globals.ts
@@ -0,0 +1,8 @@
+import { createGlobalStyle, css } from 'styled-components'
+
+const styles = css``
+const GlobalStyle = createGlobalStyle`
+  
+`
+
+export default GlobalStyle
---
+++ src/styles/theme.ts
@@ -0,0 +1,20 @@
+import { DefaultTheme } from 'styled-components'
+
+export const Theme: DefaultTheme = {
+  fontSize: {
+    global: '16px',
+    p: '1em',
+    h1: '1.25em',
+    h2: '1.25em',
+    h3: '1.25em',
+  },
+  color: {
+    text: '#000',
+    primary: 'red',
+    second: 'green',
+    white: '#fff',
+    black: '#000',
+  },
+}
+
+export default Theme
