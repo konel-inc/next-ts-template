670efdff41e790ce48d8d2353ede90e6337f562c
jay <jayq8805@gmail.com>
2022-04-25 03:01:32 +0900

    test: commitlint in husky

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,8 +3,7 @@ import type { NextPage } from 'next'
 const Home: NextPage = () => {
   return (
     <div className='page'>
-      <img src='' alt='' />
-      <img src='' alt='' />
+      <div className='inner' />
     </div>
   )
 }
