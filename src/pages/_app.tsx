7d61a2ca82e8afdbc2ee87fbb60a4fd1f652cf85
jay <jayq8805@gmail.com>
2022-04-26 08:44:29 +0900

    update(lib): google tag manager Components and script

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import type { AppProps } from 'next/app'
 import '../styles/globals.css'
-import GoogleAnalytics from 'lib/gtag'
+import { GoogleAnalytics, GoogleTagManager } from 'lib/gtag'
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
     <>
+      <GoogleTagManager />
       <GoogleAnalytics />
       <Component {...pageProps} />
     </>
