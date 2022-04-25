32c8ab3f3b2c4b987b430ff0f5d6bb3992f1b8d2
jay <jayq8805@gmail.com>
2022-04-26 07:30:00 +0900

    fix: src/_app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,9 +8,7 @@ import Theme from 'styles/theme'
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
     <>
-      <Head>
-        <GoogleAnalytics />
-      </Head>
+      <GoogleAnalytics />
       <ThemeProvider theme={Theme}>
         <Component {...pageProps} />
       </ThemeProvider>
