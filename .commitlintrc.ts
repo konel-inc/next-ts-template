442eebb92520b4884825f0b804570de0e1fcb34f
jay <jayq8805@gmail.com>
2022-05-05 09:01:09 +0900

    update: commitlint convention add 'docs'

--- .commitlintrc.ts
+++ .commitlintrc.ts
@@ -3,7 +3,7 @@ import type { UserConfig } from '@commitlint/types'
 const Config: UserConfig = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'type-enum': [2, 'always', ['test', 'fix', 'update', 'release']],
+    'type-enum': [2, 'always', ['test', 'fix', 'update', 'release', 'docs']],
     'subject-case': [0, 'always', 'sentence-case'],
   },
 }
